362ceea6ab88f16023c6a544d05077da6c6fe569
developer <developer@system76-pc.localdomain>
2025-02-21 10:59:24 -0500

    beautified the extensions modal

--- src/app/_components/SigMfComponents/Extensions/ExtensionPortal.tsx
+++ src/app/_components/SigMfComponents/Extensions/ExtensionPortal.tsx
@@ -55,25 +55,36 @@ function ModalContent({ onClose, extensions, moveData }: { onClose: Function, ex
         return retObj;
     });
 
+    const [btnEnabled, setBtnEnabled] = useState<boolean>(false);
+
     function handleData() {
         moveData(output);
         onClose();
     }
 
-    function printOutput() {
-        console.log(output);
-    }
+    useEffect(() => {
+        let validData = true;
+        Object.keys(output).forEach(obj => {
+            if (Object.values(output[obj as keyof typeof output]).includes("")) {validData = false};
+        });
+        setBtnEnabled(validData);
+    }, [output]);
 
     return (
-        <div className="grid grid-cols-1 justify-evenly items-center shadow-slate-300 bg-slate-500 border-2 absolute w-full h-full bottom-16 top-16">
-            <h1 className="text-center uppercase font-extrabold">Extensions</h1>
+        <div className="grid grid-cols-1 place-items-center justify-evenly items-center shadow-zinc-600 dark:bg-zinc-950 border-2 bg-slate-300 absolute w-full h-full bottom-16 top-16">
+            <h1 className="text-center uppercase font-extrabold text-4xl">Extensions</h1>
+            <p className="w-1/2">
+                The use of extensions requires the specification of which version of the extension is in use and whether
+                the application must support the extension. A version number must be given in order to configure. Optional
+                is presumed False unless box is checked.
+            </p>
             <div className="">
             {
                 extensions.map(ext => {
                     return (
-                        <div className="text-center uppercase font-extrabold" key={`${ext}-top-div`}>
-                            <h1>{ext} Extension</h1>
-                            <div className="grid grid-cols-2 gap-2 capitalize w-8/12 mb-8 font-normal">
+                        <div className="text-center font-extrabold dark:bg-zinc-800 bg-slate-400 rounded-lg w-fit p-2 mt-4" key={`${ext}-top-div`}>
+                            <h1 className="uppercase bg-indigo-300 dark:bg-indigo-800 rounded-sm mb-2">{ext} Extension</h1>
+                            <div className="grid grid-cols-2 gap-2 capitalize w-8/12 mb-8 last:mb-2 font-normal">
                                 <label id={`${ext}-name-label`} key={`${ext}-name-label`} className="text-right">Name</label><p>{ext}</p>
                                 <label id={`${ext}-version-label`} key={`${ext}-version-label`} className="text-right">Version </label><input type="text" id={`${ext}-version-input`} onChange={
                                     (e) => {
@@ -87,7 +98,7 @@ function ModalContent({ onClose, extensions, moveData }: { onClose: Function, ex
                                             }
                                         });
                                     }
-                                } placeholder="e.g. 1.2.3"></input>
+                                } placeholder="e.g. 1.2.3" className="text-black dark:bg-white bg-slate-200"></input>
                                 <label id={`${ext}-optional-label`} key={`${ext}-optional-label`} className="text-right">Optional? </label><input type="checkbox" id={`${ext}-optional-input`} onChange={
                                     (e) => {
                                         let tmp = output;
@@ -100,14 +111,14 @@ function ModalContent({ onClose, extensions, moveData }: { onClose: Function, ex
                                             }
                                         });
                                     }
-                                }></input>
+                                } className=""></input>
                             </div>
                         </div>
                     );
                 })
             }
             </div>
-            <button className="rounded-md bg-slate-600 hover:bg-slate-700" onClick={handleData}>Close</button>
+            <button className={`rounded-md pl-4 pr-4 ${btnEnabled ? "dark:bg-indigo-800 bg-indigo-400" : "bg-slate-200 text-slate-300 dark:bg-slate-500"} ${btnEnabled ? "dark:hover:bg-indigo-900 hover:bg-indigo-500" : ""}`} onClick={handleData} disabled={!btnEnabled}>Configure</button>
         </div>
     )
 }
