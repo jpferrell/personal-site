60d4bc733a3473156ffa210008c5d558ddc610e0
developer <developer@system76-pc.localdomain>
2025-02-21 13:21:04 -0500

    trying to fix the recursive objects mess I hath wrought

--- src/app/_components/SigMfComponents/SigMfArrayDisplay.tsx
+++ src/app/_components/SigMfComponents/SigMfArrayDisplay.tsx
@@ -13,20 +13,55 @@ export default function SigMfArrayDisplay( { inData, inIdx, typeStr, deleterFunc
         deleterFunction(idx);
     }
 
+    function createObjectList(obj: object) {
+        console.log(obj);
+        const tmp = [];
+        Object.keys(obj).map(key => {
+            console.log(key);
+            if (typeof data[obj as keyof typeof obj] === 'object') {
+                return createObjectList(obj[key as keyof typeof obj] as object);
+            } else {
+                console.log("here");
+                /*
+                const tmpArr = Object.keys(obj[key as keyof typeof obj] as object).map(subkey => {
+                    console.log("subkey:" + subkey);
+                    return (
+                        <li key={`${key}-${subkey}-key`}><em>{key}:{subkey}</em>{obj[key as keyof typeof obj][subkey] as ReactNode}</li>
+                    );
+                });
+                console.log(tmpArr);
+                return tmpArr;
+                */
+               /*
+                return (
+                    <li key={`${key}-key`}><em>{key}</em>: {obj[key as keyof typeof obj] as ReactNode}</li>
+                );
+                */
+               tmp.push(<li key={`${key}-key`}><em>{key}</em>: {obj[key as keyof typeof obj] as ReactNode}</li>);
+            }
+            console.log(tmp);
+            return tmp;
+        });
+    }
+
     return (
-        <div className="bg-slate-400 dark:hover:bg-slate-600 dark:text-stone-100 dark:hover:text-white cursor-pointer rounded-md mx-4 items-center justify-between p-4 first:mt-2 grid grid-cols-10">
-            <p className="overflow-auto col-span-3"><strong>{typeStr} {idx}</strong></p>
+        <div className="bg-slate-400 dark:hover:bg-slate-600 dark:text-stone-100 dark:hover:text-white cursor-pointer rounded-md mx-4 items-center p-4 first:mt-2 grid grid-cols-10">
+            <p className="overflow-auto col-span-3 flex justify-start"><strong>{typeStr} {idx}</strong></p>
             <div className="grid grid-cols-1 col-span-6">
                 <ul className="overflow-auto">
                     {Object.keys(data).map(key => {
-                        if (typeof data[key as keyof typeof data] === 'object') {
+                        if (typeof data[key as keyof typeof data] === 'object') { /*
                             const tmpArr = Object.keys(data[key as keyof typeof data]).map(subkey => {
                                 return (
-                                    <li key={`${key}-${subkey}-key`}><em>{key}:{subkey}</em>: {/*data[key as keyof typeof data][subkey] as ReactNode*/}</li>
+                                    <li key={`${key}-${subkey}-key`}><em>{key}:{subkey}</em> {data[key as keyof typeof data][subkey] as ReactNode}</li>
                                 )
                             });
 
-                            return tmpArr;
+                            return tmpArr; */
+                            //return createObjectList(data[key]) as ReactNode;
+                            //const whatevs = createObjectList(data[key]);
+                            //console.log(whatevs);
+                            return createObjectList(data[key]) as ReactNode;
                         } else {
                             return (
                                 <li key={`${key}-key`}><em>{key}</em>: {data[key as keyof typeof data] as ReactNode}</li>
