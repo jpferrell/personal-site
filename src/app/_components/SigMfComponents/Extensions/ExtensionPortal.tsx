28a57fc1c7d56678538dcb2f3a680f1b5d0b2472
developer <developer@system76-pc.localdomain>
2025-02-21 11:44:51 -0500

    added ability to close extensions modal without doing configuration

--- src/app/_components/SigMfComponents/Extensions/ExtensionPortal.tsx
+++ src/app/_components/SigMfComponents/Extensions/ExtensionPortal.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function ExtensionPortal( { extArr, isEnabled, moveExtObj, showPortal }: {extArr: string[], isEnabled: boolean, moveExtObj: Function, showPortal: Function} ) {
 
@@ -62,6 +63,10 @@ function ModalContent({ onClose, extensions, moveData }: { onClose: Function, ex
         onClose();
     }
 
+    function handleExit() {
+        onClose();
+    }
+
     useEffect(() => {
         let validData = true;
         Object.keys(output).forEach(obj => {
@@ -71,54 +76,59 @@ function ModalContent({ onClose, extensions, moveData }: { onClose: Function, ex
     }, [output]);
 
     return (
-        <div className="grid grid-cols-1 place-items-center justify-evenly items-center shadow-zinc-600 dark:bg-zinc-950 border-2 bg-slate-300 absolute w-full h-full bottom-16 top-16">
-            <h1 className="text-center uppercase font-extrabold text-4xl">Extensions</h1>
-            <p className="w-1/2">
-                The use of extensions requires the specification of which version of the extension is in use and whether
-                the application must support the extension. A version number must be given in order to configure. Optional
-                is presumed False unless box is checked.
-            </p>
-            <div className="">
-            {
-                extensions.map(ext => {
-                    return (
-                        <div className="text-center font-extrabold dark:bg-zinc-800 bg-slate-400 rounded-lg w-fit p-2 mt-4" key={`${ext}-top-div`}>
-                            <h1 className="uppercase bg-indigo-300 dark:bg-indigo-800 rounded-sm mb-2">{ext} Extension</h1>
-                            <div className="grid grid-cols-2 gap-2 capitalize w-8/12 mb-8 last:mb-2 font-normal">
-                                <label id={`${ext}-name-label`} key={`${ext}-name-label`} className="text-right">Name</label><p>{ext}</p>
-                                <label id={`${ext}-version-label`} key={`${ext}-version-label`} className="text-right">Version </label><input type="text" id={`${ext}-version-input`} onChange={
-                                    (e) => {
-                                        let tmp = output;
-                                        setOutput({
-                                            ...tmp,
-                                            [ext]: {
-                                                name: ext,
-                                                version: e.target.value,
-                                                optional: output[ext as keyof typeof output]!.optional
+        <div>
+            <div className="shadow-zinc-600 dark:bg-zinc-950 border-2 bg-slate-300 absolute w-full h-full bottom-16 top-16">
+                <AiOutlineClose className="fill-rose-500 flex rounded-lg dark:hover:bg-zinc-700 cursor-pointer" onClick={handleExit} />
+                <div className="grid grid-cols-1 place-items-center">
+                    <h1 className="text-center uppercase font-extrabold text-4xl">Extensions</h1>
+                    <p className="w-1/2">
+                        The use of extensions requires the specification of which version of the extension is in use and whether
+                        the application must support the extension. A version number must be given in order to configure. Optional
+                        is presumed False unless box is checked.
+                    </p>
+                    <div className="">
+                    {
+                        extensions.map(ext => {
+                            return (
+                                <div className="text-center font-extrabold dark:bg-zinc-800 bg-slate-400 rounded-lg w-fit p-2 mt-4" key={`${ext}-top-div`}>
+                                    <h1 className="uppercase bg-indigo-300 dark:bg-indigo-800 rounded-sm mb-2">{ext} Extension</h1>
+                                    <div className="grid grid-cols-2 gap-2 capitalize w-8/12 mb-8 last:mb-2 font-normal">
+                                        <label id={`${ext}-name-label`} key={`${ext}-name-label`} className="text-right">Name</label><p>{ext}</p>
+                                        <label id={`${ext}-version-label`} key={`${ext}-version-label`} className="text-right">Version </label><input type="text" id={`${ext}-version-input`} onChange={
+                                            (e) => {
+                                                let tmp = output;
+                                                setOutput({
+                                                    ...tmp,
+                                                    [ext]: {
+                                                        name: ext,
+                                                        version: e.target.value,
+                                                        optional: output[ext as keyof typeof output]!.optional
+                                                    }
+                                                });
                                             }
-                                        });
-                                    }
-                                } placeholder="e.g. 1.2.3" className="text-black dark:bg-white bg-slate-200"></input>
-                                <label id={`${ext}-optional-label`} key={`${ext}-optional-label`} className="text-right">Optional? </label><input type="checkbox" id={`${ext}-optional-input`} onChange={
-                                    (e) => {
-                                        let tmp = output;
-                                        setOutput({
-                                            ...tmp,
-                                            [ext]: {
-                                                name: ext,
-                                                version: output[ext as keyof typeof output]!.version,
-                                                optional: e.target.checked
+                                        } placeholder="e.g. 1.2.3" className="text-black dark:bg-white bg-slate-200"></input>
+                                        <label id={`${ext}-optional-label`} key={`${ext}-optional-label`} className="text-right">Optional? </label><input type="checkbox" id={`${ext}-optional-input`} onChange={
+                                            (e) => {
+                                                let tmp = output;
+                                                setOutput({
+                                                    ...tmp,
+                                                    [ext]: {
+                                                        name: ext,
+                                                        version: output[ext as keyof typeof output]!.version,
+                                                        optional: e.target.checked
+                                                    }
+                                                });
                                             }
-                                        });
-                                    }
-                                } className=""></input>
-                            </div>
-                        </div>
-                    );
-                })
-            }
+                                        } className=""></input>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+                    </div>
+                    <button className={`rounded-md pl-4 pr-4 ${btnEnabled ? "dark:bg-indigo-800 bg-indigo-400" : "bg-slate-200 text-slate-300 dark:bg-slate-500"} ${btnEnabled ? "dark:hover:bg-indigo-900 hover:bg-indigo-500" : ""}`} onClick={handleData} disabled={!btnEnabled}>Configure</button>
+                </div>
             </div>
-            <button className={`rounded-md pl-4 pr-4 ${btnEnabled ? "dark:bg-indigo-800 bg-indigo-400" : "bg-slate-200 text-slate-300 dark:bg-slate-500"} ${btnEnabled ? "dark:hover:bg-indigo-900 hover:bg-indigo-500" : ""}`} onClick={handleData} disabled={!btnEnabled}>Configure</button>
         </div>
-    )
+    );
 }
